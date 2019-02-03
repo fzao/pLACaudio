6fd8368706456cd131a333727d7fcb5bde7a6be7
Fabrice Zaoui <trevignon.finistere@gmail.com>
2019-02-03 09:01:00 +0100

    add GPL info in header file

--- pLACaudio.py
+++ pLACaudio.py
@@ -25,6 +25,21 @@ pLAC-Audio supposes the presence of the FFmpeg executable in the PATH environmen
  | |
  |_|
 
+This file is part of pLAC-audio.
+
+pLAC-audio is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+
+pLAC-audio is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with pLAC-audio.  If not, see <http://www.gnu.org/licenses/>
+
 Copyright (c) 2019 Fabrice Zaoui
 
 License GNU GPL v3
