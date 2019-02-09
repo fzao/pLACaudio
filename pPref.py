f4a670bcc674ad651449536243bceec96b228ba4
fabrice zaoui <trevignon.finistere@gmail.com>
2019-02-09 19:09:55 +0100

    necessary quit button

--- pPref.py
+++ pPref.py
@@ -27,7 +27,7 @@ Copyright (c) 2019 Fabrice Zaoui
 License GNU GPL v3
 
 """
-from PyQt5.QtWidgets import QMainWindow, QCheckBox, QWidget
+from PyQt5.QtWidgets import QMainWindow, QCheckBox, QPushButton
 from PyQt5.QtGui import QIcon
 from PyQt5 import Qt
 
@@ -35,17 +35,26 @@ from PyQt5 import Qt
 class Preference(QMainWindow):
     def __init__(self, parent):
         super(Preference, self).__init__(parent)
-        self.initUI()
-
-    def initUI(self):
+        # window
         self.setWindowTitle('Preferences')
         self.setWindowIcon(QIcon('./icon/beer.ico'))
         self.setWindowModality(Qt.Qt.WindowModal)
         self.setFixedSize(400, 400)
+        # checkbox (dark theme)
         self.style = QCheckBox('Dark theme', self)
+        # quit button
+        self.btn_ok = QPushButton('OK', self)
+        self.initUI()
+
+    def initUI(self):
+        # checkbox (dark theme)
         self.style.move(25, 25)
         self.style.resize(200, 50)
         self.style.stateChanged.connect(self.changeStyle)
+        # quit button
+        self.btn_ok.clicked.connect(self.pref_exit)
+        self.btn_ok.resize(150,50)
+        self.btn_ok.move(125, 340)
 
     def changeStyle(self):
         if self.style.isChecked():
@@ -53,7 +62,8 @@ class Preference(QMainWindow):
         else:
             self.parent().setStyleSheet('')
 
-
+    def pref_exit(self):
+        self.close()
 
 
 
